068f6668450cdf7d2748a78c66ebea41bac5a4eb
vejas <118309632+Las-Vejas@users.noreply.github.com>
2025-12-30 12:55:38 +0100

    git init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from "mode-watcher";
+	import { Button } from "$lib/components/ui/button/index.js";
+    import { toggleMode } from "mode-watcher";
+    import SunIcon from "@lucide/svelte/icons/sun";
+    import MoonIcon from "@lucide/svelte/icons/moon";
+	import { resetMode, setMode } from "mode-watcher";
+  	import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+  	import { buttonVariants } from "$lib/components/ui/button/index.js";
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<ModeWatcher />
+<nav class="sticky top-0 z-50 backdrop-blur-sm border-b">
+	<div class="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+		<a href="/" class="text-2xl font-bold tracking-tight hover:shadow-accent transition-colors">
+			longy
+		</a>
+		<div class="flex gap-2">
+			<Button href="/dashboard" variant="default">Dashboard</Button>
+        	<Button onclick={toggleMode} variant="outline" size="icon">
+            	<SunIcon class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"/>
+            	<MoonIcon class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"/>
+            	<span class="sr-only">Toggle theme</span>
+        	</Button>
+		</div>
+    </div>
+</nav>
+
+
+<main class="flex-1  min-h-[calc(100vh-16rem)]">
+	{@render children()}
+</main>
+
+<footer class="border-t border-zinc-300 dark:border-zinc-700">
+		<div class="max-w-7xl mx-auto px-6 py-8">
+			<div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+
+				<div>
+					<h3 class="text-lg font-bold mb-3">shawty</h3>
+					<p class="text-sm ">
+						Simple and short links.
+					</p>
+				</div>
+
+				<div>
+					<h4 class="text-sm font-semibold mb-3">Links</h4>
+					<ul class="space-y-2 text-sm">
+						<li><a href="/" class="hover:text-zinc-700 hover:dark:text-zinc-200 transition-colors">Home</a></li>
+						<li><a href="/dashboard" class="hover:text-zinc-700 hover:dark:text-zinc-200 transition-colors">Dashboard</a></li>
+
+					</ul>
+				</div>
+
+				<div>
+					<h4 class="text-sm font-semibold mb-3">Connect</h4>
+					<ul class="space-y-2 text-sm">
+						<li><a href="https://hackclub.enterprise.slack.com/team/U0930DMR4BA" class="hover:text-zinc-700 hover:dark:text-zinc-200 transition-colors">@Vejas on Slack</a></li>
+						<li><a href="https://github.com/las-vejas/longy" class="hover:text-zinc-700 hover:dark:text-zinc-200 transition-colors">GitHub Repo</a></li>
+					</ul>
+				</div>	
+		</div>
+		<!-- Bottom bar -->
+		<div class="mt-8 pt-6 border-t border-zinc-300 dark:border-zinc-700 flex items-center justify-between text-sm text-zinc-400">
+			<p>&copy; {new Date().getFullYear()} shawty. Built with ❤️ by <a href="https://vejas.zip" class="text-decoration-underline">Vejas</a></p>
+			<DropdownMenu.Root>
+  					<DropdownMenu.Trigger class={buttonVariants({ variant: "outline", size: "icon" })}>
+						<SunIcon class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"/>
+						<MoonIcon class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"/>
+    					<span class="sr-only">Toggle theme</span>
+  					</DropdownMenu.Trigger>
+  					<DropdownMenu.Content align="end">
+    					<DropdownMenu.Item onclick={() => setMode("light")}>Light</DropdownMenu.Item>
+    					<DropdownMenu.Item onclick={() => setMode("dark")}>Dark</DropdownMenu.Item>
+    					<DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
+  					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+		</div>
+	</footer> 
+				
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<div class="relative overflow-hidden">
+    <!-- Hero Section -->
+    <div class="relative px-6 py-20 lg:py-32">
+        <!-- Background gradient -->
+        <div class="absolute inset-0 -z-10 bg-linear-to-br from-blue-500 via-transparent to-purple-500/50"></div>
+        
+        <div class="mx-auto max-w-5xl text-center space-y-8">
+        <!-- Hero Text -->
+            <h1 class="text-5xl lg:text-7xl font-bold tracking-tight">
+                <span class="bg-linear-to-r from-black to-zinc-700 dark:from-white dark:to-zinc-400 bg-clip-text text-transparent">
+                    Long links,
+                </span>
+                <br />
+                <span class="bg-linear-to-r from-amber-400 to-sky-400 bg-clip-text text-transparent">
+                    finite possibilities <span class="text-2xl font-bold tracking-tight">jk</span>
+                </span>
+            </h1>
+        </div>
+    </div>
+</div>
